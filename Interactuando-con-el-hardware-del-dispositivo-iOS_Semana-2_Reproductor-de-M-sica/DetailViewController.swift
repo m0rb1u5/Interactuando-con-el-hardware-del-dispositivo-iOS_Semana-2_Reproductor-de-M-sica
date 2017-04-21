42ddc28e847adcdaffb6255df6c5092ae58fcab9
m0rb1u5 <goesrex@gmail.com>
2017-04-21 08:39:26 -0500

    Initial Commit

---
+++ Interactuando-con-el-hardware-del-dispositivo-iOS_Semana-2_Reproductor-de-M-sica/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  Interactuando-con-el-hardware-del-dispositivo-iOS_Semana-2_Reproductor-de-M-sica
+//
+//  Created by Juan Carlos Carbajal Ipenza on 21/04/17.
+//  Copyright © 2017 Juan Carlos Carbajal Ipenza. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
---
+++ Interactuando-con-el-hardware-del-dispositivo-iOS_Semana-2_Reproductor-de-M-sicaTests/Interactuando_con_el_hardware_del_dispositivo_iOS_Semana_2_Reproductor_de_M_sicaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Interactuando_con_el_hardware_del_dispositivo_iOS_Semana_2_Reproductor_de_M_sicaTests.swift
+//  Interactuando-con-el-hardware-del-dispositivo-iOS_Semana-2_Reproductor-de-M-sicaTests
+//
+//  Created by Juan Carlos Carbajal Ipenza on 21/04/17.
+//  Copyright © 2017 Juan Carlos Carbajal Ipenza. All rights reserved.
+//
+
+import XCTest
+@testable import Interactuando_con_el_hardware_del_dispositivo_iOS_Semana_2_Reproductor_de_M_sica
+
+class Interactuando_con_el_hardware_del_dispositivo_iOS_Semana_2_Reproductor_de_M_sicaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
